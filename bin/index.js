289e9a677c320aa53f5478c7b297a0491ba82c91
beibeidan <beibeidan18@163.com>
2020-09-29 16:21:57 +0800

    feat: inquirer and change package

--- bin/index.js
+++ bin/index.js
@@ -28,14 +28,24 @@ program.command('init <name>')
     .description('a simple cli')
     .action(async name => {
         await inquirer.prompt(dataList).then(answer => {
-            console.log(answer);
-            console.log(`${ process.cwd() }`);
-            // fs.readFile(`${process.cwd()}/${name}/package.json`, (err, data) => {
-            //     if (err) {
-            //         console.log('读取文件有问题' + err);
-            //         throw err;
-            //     }
-            // })
+            //重写package.json  ${process.cwd()}获取当前路径
+            fs.readFile(`${process.cwd()}/package.json`, (err, data) => {
+                if (err) {
+                    console.log('读取文件有问题' + err);
+                    throw err;
+                }
+                let _data = JSON.parse(data.toString());
+                console.log(_data);
+                _data.name = answer.name;
+                _data.version = '1.0.0';
+                _data.author = answer.author;
+                let str = JSON.stringify(_data, null, 4);
+                fs.writeFile(`${process.cwd()}/package.json`, str, (err) => {
+                    if (err) {
+                        throw err;
+                    }
+                })
+            });
             // const packagePath = `${name}/package.json`;
             // const packageJson = fs.readFileSync(packagePath, 'utf-8');
             // const packageResult = handlebars.compile(packageJson)(answer);
@@ -45,7 +55,7 @@ program.command('init <name>')
                 console.log(err);
                 return;
             }
-        })
+        });
         await init(name);
     });
 program.parse(process.argv);
